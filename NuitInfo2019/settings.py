cd11ee32172119f94cf82b314bc9020058ffb7b0
Matthieu <lourme78@gmail.colm>
2019-12-05 11:34:34 +0100

    Tuto Django fini

--- NuitInfo2019/settings.py
+++ NuitInfo2019/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'NuitInfo2019.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nuitinfo2',
+        'USER': 'mat',
+        'PASSWORD': 'azerty',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
